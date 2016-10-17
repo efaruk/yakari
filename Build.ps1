2150d1014402655cb924243bd2f183eee860ce3a
efaruk <faruk.pehlivanli@gmail.com>
2016-10-17 18:36:44 +0300

    try to fix appveyor build, run tests

--- Build.ps1
+++ Build.ps1
@@ -59,7 +59,7 @@ Invoke-MSBuild .\src\Yakari.sln
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-#exec { & dotnet test .\src\Yakari.Tests -c Release }
+exec { & dotnet test .\src\Yakari.Tests -c Release }
 
 exec { & dotnet pack .\src\Yakari -c Release -o .\artifacts --version-suffix=$revision }
 
