606a40fe17b076939b97f3766e407e8725fd0e7c
efaruk <faruk.pehlivanli@gmail.com>
2016-10-17 17:30:45 +0300

    try to fix appveyor build, fix sln path

--- Build.ps1
+++ Build.ps1
@@ -54,7 +54,7 @@ EnsurePsbuildInstalled
 
 exec { & dotnet restore }
 
-Invoke-MSBuild -Path .\src\Yakari.sln
+Invoke-MSBuild .\src\Yakari.sln
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
