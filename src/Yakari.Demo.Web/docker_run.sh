a1f84b69e6f6a3ab1383c1a3fb7b84c5531536e7
efaruk <faruk.pehlivanli@gmail.com>
2016-11-02 19:08:50 +0300

    docker command edit

--- src/Yakari.Demo.Web/docker_run.sh
+++ src/Yakari.Demo.Web/docker_run.sh
@@ -4,4 +4,6 @@ docker run --name yakariweb0 -d -p 8880:80 efaruk/yakari:web
 docker run --name yakariweb1 -d -p 8881:80 efaruk/yakari:web
 docker run --name yakariweb2 -d -p 8882:80 efaruk/yakari:web
 docker run --name yakariweb3 -d -p 8883:80 efaruk/yakari:web
+docker run --name yakariweb4 -d -p 8884:80 efaruk/yakari:web
+docker run --name yakariweb6 -d -p 8885:80 efaruk/yakari:web
 
---
+++ src/Yakari.Demo.Web/docker_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop yakariweb0 yakariweb1 yakariweb2 yakariweb3 yakariweb4 yakariweb5
+docker stop redis
